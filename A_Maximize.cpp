67a72cf6d24175f879dc5cd6b7e15d25b038bf76
Apurbo <shahapurbo007@gmail.com>
2024-05-03 11:04:26 +0600

    Problem A of Div 3

---
+++ A_Maximize.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define pb push_back
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+
+        int x;
+        cin >> x;
+
+        cout << x - 1 << endl;
+    }
+}
