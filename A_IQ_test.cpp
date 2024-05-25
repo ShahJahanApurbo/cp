d8fb1f7b815c1cb1b3492a79b428fb90182fc4af
Md Shajahan Apurba <shahapurbo007@gmail.com>
2024-05-25 19:19:35 +0600

    CP day 4

---
+++ A_IQ_test.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio()                      \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nline "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+// typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
+
+#ifndef ONLINE_JUDGE
+#include "apurbo.h"
+#define debug(x)       \
+    cerr << #x << " "; \
+    _print(x);         \
+    cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    int ans = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] & 1)
+            ans++;
+    }
+
+    if (ans == 1)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            if (a[i] & 1)
+            {
+                cout << i + 1 << endl;
+                return;
+            }
+        }
+    }
+    else
+    {
+        for (int i = 0; i < n; i++)
+        {
+            if (!(a[i] & 1))
+            {
+                cout << i + 1 << endl;
+                return;
+            }
+        }
+    }
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("Error.txt", "w", stderr);
+#endif
+
+    ll t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
---
+++ compilerlabmanul8.c
@@ -0,0 +1,75 @@
+/* Calculate FIRST of a regular expression. */
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char t[5], nt[10], p[5][5], first[5][5], temp;
+    int i, j, note, nont, k = 0, f = 0;
+
+    printf("\nEnter the no. of Non-terminals in the grammar: ");
+    scanf("%d", &nont);
+
+    printf("\nEnter the Non-terminals in the grammar:\n");
+    for (i = 0; i < nont; i++) {
+        scanf("\n%c", &nt[i]);
+    }
+
+    printf("\nEnter the no. of Terminals in the grammar: (Enter e for epsilon): ");
+    scanf("%d", &note);
+
+    printf("\nEnter the Terminals in the grammar:\n");
+    for (i = 0; i < note || t[i] == '$'; i++) {
+        scanf("\n%c", &t[i]);
+    }
+
+    for (i = 0; i < nont; i++) {
+        p[i][0] = nt[i];
+        first[i][0] = nt[i];
+    }
+
+    printf("\nEnter the productions:\n");
+    for (i = 0; i < nont; i++) {
+        scanf("%c", &temp);
+        printf("\nEnter the production for %c (End the production with '$' sign): ", p[i][0]);
+        for (j = 0; p[i][j] != '$';) {
+            j += 1;
+            scanf("%c", &p[i][j]);
+        }
+    }
+
+    for (i = 0; i < nont; i++) {
+        printf("\nThe production for %c -> ", p[i][0]);
+        for (j = 1; p[i][j] != '$'; j++) {
+            printf("%c", p[i][j]);
+        }
+    }
+
+    for (i = 0; i < nont; i++) {
+        f = 0;
+        for (j = 1; p[i][j] != '$'; j++) {
+            for (k = 0; k < note; k++) {
+                if (f == 1) break;
+                if (p[i][j] == t[k]) {
+                    first[i][j] = t[k];
+                    first[i][j + 1] = '$';
+                    f = 1;
+                    break;
+                } else if (p[i][j] == nt[k]) {
+                    first[i][j] = first[k][j];
+                    if (first[i][j] == 'e') continue;
+                    first[i][j + 1] = '$';
+                    f = 1;
+                    break;
+                }
+            }
+        }
+    }
+
+    for (i = 0; i < nont; i++) {
+        printf("\n\nThe first of %c -> ", first[i][0]);
+        for (j = 1; first[i][j] != '$'; j++) {
+            printf("%c\t", first[i][j]);
+        }
+    }
+    return 0;
+}
